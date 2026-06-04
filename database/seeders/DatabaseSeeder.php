3a0fb35a37e65e63851a7a1e035ff670088ac8a8
Ucin-tech <muchsinmaulana0@gmail.com>
2026-06-05 00:03:26 +0800

    fix duplicate role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Role;
-use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,48 +13,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Role::create([
-            'name' => 'superadmin',
-            'description' => 'Administrator dan creator dengan akses penuh',
-        ]);
+        // Roles
+        Role::firstOrCreate(
+            ['name' => 'superadmin'],
+            ['description' => 'Administrator dan creator dengan akses penuh']
+        );
 
-        Role::create([
-            'name' => 'admin',
-            'description' => 'Administrator dan creator dengan akses terbatas',
-        ]);
+        Role::firstOrCreate(
+            ['name' => 'admin'],
+            ['description' => 'Administrator dan creator dengan akses terbatas']
+        );
 
-        Role::create([
-            'name' => 'author',
-            'description' => 'Creator dengan akses terbatas',
-        ]);
+        Role::firstOrCreate(
+            ['name' => 'author'],
+            ['description' => 'Creator dengan akses terbatas']
+        );
 
-        Role::create([
-            'name' => 'user',
-            'description' => 'Pengguna reguler',
-        ]);
+        Role::firstOrCreate(
+            ['name' => 'user'],
+            ['description' => 'Pengguna reguler']
+        );
 
-        User::create([
-            'NIK' => 'LTG000000',
-            'name' => 'Super Admin',
-            'role' => 'superadmin',
-            'email' => 'superadmin@gmail.com',
-            'password' => bcrypt('superadmin1234'),
-        ]);
+        // Users
+        User::firstOrCreate(
+            ['email' => 'superadmin@gmail.com'],
+            [
+                'NIK' => 'LTG000000',
+                'name' => 'Super Admin',
+                'role' => 'superadmin',
+                'password' => bcrypt('superadmin1234'),
+            ]
+        );
 
-        User::create([
-            'NIK' => 'LTG000010',
-            'name' => 'Admin',
-            'role' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin1234'),
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'NIK' => 'LTG000010',
+                'name' => 'Admin',
+                'role' => 'admin',
+                'password' => bcrypt('admin1234'),
+            ]
+        );
 
-        User::create([
-            'NIK' => 'LTG0000010',
-            'name' => 'Author',
-            'role' => 'author',
-            'email' => 'author@gmail.com',
-            'password' => bcrypt('author1234'),
-        ]);
+        User::firstOrCreate(
+            ['email' => 'author@gmail.com'],
+            [
+                'NIK' => 'LTG0000010',
+                'name' => 'Author',
+                'role' => 'author',
+                'password' => bcrypt('author1234'),
+            ]
+        );
     }
 }
