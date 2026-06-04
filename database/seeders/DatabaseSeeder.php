ab5fb8e7c9eed4382c4b7656b02358dd71b170e4
Ucin-tech <muchsinmaulana0@gmail.com>
2026-06-04 23:14:06 +0800

    Remove factories from seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,58 +1,46 @@
-<?php
-
-namespace Database\Seeders;
-
-use App\Models\User;
-use App\Models\Role;
-use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
+public function run(): void
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-        Role::factory()->create([
-            'name' => 'superadmin',
-            'description' => 'Administrator dan creator dengan akses penuh',
-        ]);
-        Role::factory()->create([
-            'name' => 'admin',
-            'description' => 'Administrator dan creator dengan akses terbatas',
-        ]);
-        Role::factory()->create([
-            'name' => 'author',
-            'description' => 'Creator dengan akses terbatas',
-        ]);
-        Role::factory()->create([
-            'name' => 'user',
-            'description' => 'Pengguna reguler',
-        ]);
+    Role::create([
+        'name' => 'superadmin',
+        'description' => 'Administrator dan creator dengan akses penuh',
+    ]);
 
-        User::factory()->create([
-            'NIK' => 'LTG000000',
-            'name' => 'Super Admin',
-            'role' => 'superadmin',
-            'email' => 'superadmin@gmail.com',
-            'password' => bcrypt('superadmin1234'),
-        ]);
+    Role::create([
+        'name' => 'admin',
+        'description' => 'Administrator dan creator dengan akses terbatas',
+    ]);
 
-        User::factory()->create([
-            'NIK' => 'LTG000010',
-            'name' => 'Admin',
-            'role' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('admin1234'),
-        ]);
+    Role::create([
+        'name' => 'author',
+        'description' => 'Creator dengan akses terbatas',
+    ]);
 
-        User::factory()->create([
-            'NIK' => 'LTG0000010',
-            'name' => 'Author',
-            'role' => 'author',
-            'email' => 'author@gmail.com',
-            'password' => bcrypt('author1234'),
-        ]);
-    }
+    Role::create([
+        'name' => 'user',
+        'description' => 'Pengguna reguler',
+    ]);
+
+    User::create([
+        'NIK' => 'LTG000000',
+        'name' => 'Super Admin',
+        'role' => 'superadmin',
+        'email' => 'superadmin@gmail.com',
+        'password' => bcrypt('superadmin1234'),
+    ]);
+
+    User::create([
+        'NIK' => 'LTG000010',
+        'name' => 'Admin',
+        'role' => 'admin',
+        'email' => 'admin@gmail.com',
+        'password' => bcrypt('admin1234'),
+    ]);
+
+    User::create([
+        'NIK' => 'LTG0000010',
+        'name' => 'Author',
+        'role' => 'author',
+        'email' => 'author@gmail.com',
+        'password' => bcrypt('author1234'),
+    ]);
 }
